a88398fd01a236f31af3c1534bb1efe2d2d832d0
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2021-12-24 17:05:55 -0300

    Melhorias no cad de postagem

--- lib/components/post_card.dart
+++ lib/components/post_card.dart
@@ -14,33 +14,28 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
       padding: const EdgeInsets.all(5),
+      margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
       child: Column(
         children: [
           //CABEÇALHO
           HeadPost(post: post),
 
           //IMAGEM
-          Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: CachedNetworkImage(
-                imageUrl: post.urlImage,
-              ),
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: CachedNetworkImage(
+              imageUrl: post.urlImage,
             ),
           ),
 
           //INTERAÇÕES
           Container(
             padding: const EdgeInsets.all(5),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
-              ),
-            ),
             child: Column(
               children: [
                 Padding(
@@ -111,12 +106,6 @@ class HeadPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-          )),
       padding: const EdgeInsets.all(10),
       child: Row(
         children: [
